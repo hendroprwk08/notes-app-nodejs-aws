42fbc2c2af6c64b4cea3b1af078af175f8065e62
hendroprwk08 <40080703+hendroprwk08@users.noreply.github.com>
2023-03-08 20:21:32 +0700

    ok

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const init = async () => {
     const server = Hapi.server({
         port: 5000,
         host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+        routes: {
+            cors: {
+              origin: ['*'],
+            },
+          },
     });
 
     // 1 route
