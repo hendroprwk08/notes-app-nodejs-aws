40f3678bd107c2e8e10bb7019752e184481900a6
hendroprwk08 <40080703+hendroprwk08@users.noreply.github.com>
2023-03-08 17:27:43 +0700

    ok

--- server.js
+++ server.js
@@ -1,1 +1,44 @@
-console.log('Halo, test kita akan belajar membuat server menggunakan Hapi');
+const Hapi = require('@hapi/hapi');
+const routes = require('./routes');
+
+const init = async () => {
+    const server = Hapi.server({
+        port: 5000,
+        host: 'localhost',
+    });
+
+    // 1 route
+    // server.route({
+    //     method: 'GET',
+    //     path: '/',
+    //     handler: (request, h) => {
+    //         return 'Hello World!';
+    //     }
+    // });
+
+    // beberapa route
+    // server.route([
+    //     {
+    //         method: 'GET',
+    //         path: '/',
+    //         handler: (request, h) => {
+    //             return 'Homepage';
+    //         },
+    //     },
+    //     {
+    //         method: 'GET',
+    //         path: '/about',
+    //         handler: (request, h) => {
+    //             return 'About Page';
+    //         },
+    //     },
+    // ]);
+
+    // 3. menggunakan modul
+    server.route(routes);
+
+    await server.start();
+    console.log(`Server berjalan pada ${server.info.uri}`);
+}
+
+init();
